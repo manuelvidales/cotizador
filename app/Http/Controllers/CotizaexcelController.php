b11a884948944dd1c2ae173d97854f7a9a55ffb4
manuelvidales <virmanuel@gmail.com>
2020-03-31 17:01:49 -0500

    first

---
+++ app/Http/Controllers/CotizaexcelController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use Illuminate\Support\Facades\Storage;
+
+class CotizaexcelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       
+        $contado = '449.01';
+        $inicial = '29';
+        $plazo = '12';
+
+        $inputFileType = 'Xlsx';
+        $reader = IOFactory::createReader($inputFileType);
+        $reader->setLoadSheetsOnly(["BARATODO_TV","Cotizador"]);
+        $spreadsheet = $reader->load("storage/cotiza.xlsx");
+        $sheet = $spreadsheet->getActiveSheet();
+        $sheet->setCellValue('E4', $contado);
+        $sheet->setCellValue('E5', $inicial);
+        $sheet->setCellValue('E7', $plazo);
+        
+        $dataArray = $spreadsheet->getActiveSheet()->rangeToArray('E4:E11', NULL, TRUE, TRUE, TRUE);
+
+       dd($dataArray);
+       //echo $dataArray;
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
