98b2603b701767ab7bf019623cee2c05b4ac2c27
manuelvidales <virmanuel@gmail.com>
2020-04-01 16:48:35 -0500

    formulario cotizar

--- resources/views/cotizador/index.blade.php
+++ resources/views/cotizador/index.blade.php
@@ -7,34 +7,32 @@
             <div class="card">
                 <div class="card-header">
                     <div class="alert alert-success text-center" role="alert">
-                      <h3><strong>COTIZADOR</strong></h3>
+                      <h3><strong>COTIZADOR </strong></h3>
                        </div>
                 </div>
                 <div class="card-body">
-
                     <form id="formcotizar">
-
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                           <span class="input-group-text" >PRECIO CONTADO</span>
                           <span class="input-group-text">$</span>
                         </div>
-                        <input type="text" class="form-control" id="contado" name="contado" placeholder="Ingresar cantidad" aria-label="preciocontado" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" id="contado" name="contado" placeholder="Ingresar cantidad" >
                     </div>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                           <span class="input-group-text">INICIAL</span>
                           <span class="input-group-text">$</span>
                         </div>
-                        <input type="text" class="form-control" id="inicial" name="inicial" placeholder="Ingresar cantidad" aria-label="inicial" aria-describedby="basic-addon2">
+                        <input type="text" class="form-control" id="inicial" name="inicial" placeholder="Ingresar cantidad" >
                     </div>
                     <div class="input-group mb-3">
                         <div class="input-group-prepend">
                           <label class="input-group-text" >PLAZO</label>
                           <span class="input-group-text">#</span>
                         </div>
-                        <select class="custom-select" id="plazo" name="plazo">
-                          <option selected>Selecionar...</option>
+                        <select class="custom-select" id="plazo" name="plazo" >
+                          <option selected >Selecionar...</option>
                           <option value="1">3</option>
                           <option value="6">6</option>
                           <option value="9">9</option>
@@ -49,19 +47,19 @@
                           <option value="36">36</option>
                         </select>
                     </div>
-                    <button type="submit" class="btn btn-success"> CALCULAR CUOTAS </button>
-                    </form>
-<hr>
-                    <div class="card-footer" >
-                        <div id="resultados"> </div>
-
-                    </div>
-
+                  <div class="d-flex justify-content-center">
+                    <button type="submit" class="btn btn-success btn-lg btn-block"> CALCULAR CUOTAS <i class="fas fa-calculator fa-lg"></i></button>
+                  </div>
+                </form>
+              <hr>
+              <div class="card-footer" >
+                <div id="resultados"> </div>
+              </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
+<script src="{{ asset('js/cotizar.js')}}" ></script>
 </div>
 @endsection
